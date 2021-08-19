e8bd4892fd5e5e8dac4d5d03780e0e8ff6b1c800
DisistiNao <gabrielsaldanha373@gmail.com>
2021-08-19 12:14:29 -0300

    Desafio MegaSena - parte final

--- src/components/megaSena/DisplaySorteio.jsx
+++ src/components/megaSena/DisplaySorteio.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 
 import Sorteio from './Sorteio';
+import "./DisplaySorteio.css"
 
 export default (props) => {
     const [lista, setLista] = useState([])
@@ -9,25 +10,31 @@ export default (props) => {
         setLista(lista)
     }
 
+    console.log(lista)
+
     return (
-        <div>
+        <div className="display">
             <h3>Números sorteados:</h3>
             
             {/* TENHO QUE FORMATAR OS VALORES RECEBIDOS DA LISTA AQUI*/}
+            <span>{lista[0]}</span>
+            <span>{lista[1]}</span>
+            <span>{lista[2]}</span>
+            <span>{lista[3]}</span>
+            <span>{lista[4]}</span>
+            <span>{lista[5]}</span>
+            <span>{lista[6]}</span>
+
+            <Sorteio 
+                numsSorteio={props.numsSorteio} 
+                min={props.min} 
+                max={props.max}
+                alterarLista={alterarLista}
+            />
+                
+
             <div>
-                <Sorteio 
-                    numsSorteio={props.numsSorteio} 
-                    min={props.min} 
-                    max={props.max}
-                    alterarLista={alterarLista}
-                />
-                <span>{lista[0]}</span>
-                <span> - {lista[1]}</span>
-                <span> - {lista[2]}</span>
-                <span> - {lista[3]}</span>
-                <span> - {lista[4]}</span>
-                <span> - {lista[5]}</span>
-                <span> - {lista[6]}</span>
+                
             </div>
         </div>
     )
